a620d756d0a7fdcb9d459c7053ca16cae607a41c
RAFH82 <hrechkar@gmail.com>
2020-11-24 01:45:13 +0000

    Completed exercise

---
+++ 2_queries_joins/0_student_rollovers.sql
@@ -0,0 +1,9 @@
+SELECT 
+students.name,
+cohorts.name,
+cohorts.start_date AS cohort_start_date, 
+students.start_date AS student_start_date
+FROM students 
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohort_start_date;
